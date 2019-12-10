add917d1d063bfeae5969ef347cfe5bdfb41ef06
bbuck1 <bbuck@masonlive.gmu.edu>
2019-12-10 11:35:39 -0500

    further attempt to implement depedent dropdowns

--- call/forms.py
+++ call/forms.py
@@ -1,2 +1,20 @@
 from django import forms
-from .models import Post
+from .models import Post, State, Country
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'call_date', 'denomination', 'country','state','city', 'body')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['state'].queryset = State.objects.none()
+
+        if 'country' in self.data:
+            try:
+                country_id = int(self.data.get('country'))
+                self.fields['state'].queryset = State.objects.filter(country_id=country_id).order_by('name')
+            except (ValueError, TypeError):
+                pass  # invalid input from the client; ignore and fallback to empty City queryset
+        elif self.instance.pk:
+            self.fields['state'].queryset = self.instance.country.state_set.order_by('name')
